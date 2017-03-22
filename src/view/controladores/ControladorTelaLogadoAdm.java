501719530e6f9185f77d12e86cd77f5fba9ecb1a
sidronio <lucas_sidronio@yahoo.com.br>
2017-03-22 01:31:32 -0300

    Implementação da tela de ADM

--- src/view/controladores/ControladorTelaLogadoAdm.java
+++ src/view/controladores/ControladorTelaLogadoAdm.java
@@ -1,10 +1,16 @@
 package view.controladores;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import negocio.controladores.Contexto;
 
 public class ControladorTelaLogadoAdm extends AnchorPane{
 	
+	
+	@FXML
+	private Label lbUser;
 	
 	public ControladorTelaLogadoAdm(){
 		try{
@@ -12,6 +18,9 @@ public class ControladorTelaLogadoAdm extends AnchorPane{
 			
 			loader.setController(this);
 			this.getChildren().add(loader.load());
+			
+			
+			this.lbUser.setText("Usuário: " +Contexto.getIntance().getUsuarioLogado().getNome());
 		}catch(Exception e){
 			e.printStackTrace();
 		}
